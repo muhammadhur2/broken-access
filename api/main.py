7dfd2039f8aa01a2816c7190a7f221aaffbed092
Muhammad Hur <muhammedhur@gmail.com>
2023-12-30 21:43:31 +0500

    mongodb

--- api/main.py
+++ api/main.py
@@ -4,9 +4,11 @@ from datetime import datetime, timedelta
 from flask_mail import Mail, Message
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+mongo_uri = "mongodb+srv://hur:2Rm4Ens6QujrtPUS@cluster0.ljc5i8j.mongodb.net/?retryWrites=true&w=majority"
+client = MongoClient(mongo_uri)
+db = client.get_default_database()  # This will get the default database or you can specify a database name
+
 app.config['SECRET_KEY'] = 'your_secret_key'
-db = SQLAlchemy(app)
 
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 587
@@ -17,18 +19,18 @@ app.config['MAIL_USE_SSL'] = False
 
 mail = Mail(app)
 
-class UserActivity(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-    activity = db.Column(db.Text)
-    timestamp = db.Column(db.DateTime, default=datetime.utcnow)
+# class UserActivity(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+#     activity = db.Column(db.Text)
+#     timestamp = db.Column(db.DateTime, default=datetime.utcnow)
 
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True, nullable=False)
-    password = db.Column(db.String(80), nullable=False)
-    activities = db.relationship('UserActivity', backref='user', lazy=True)
-    is_admin = db.Column(db.Boolean, default=False)
+# class User(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     username = db.Column(db.String(80), unique=True, nullable=False)
+#     password = db.Column(db.String(80), nullable=False)
+#     activities = db.relationship('UserActivity', backref='user', lazy=True)
+#     is_admin = db.Column(db.Boolean, default=False)
 
 @app.route('/')
 def index():
@@ -38,12 +40,9 @@ def index():
 def signup():
     if request.method == 'POST':
         username = request.form['username']
-        password = request.form['password']
-        new_user = User(username=username, password=password)
-
-        db.session.add(new_user)
-        db.session.commit()
-
+        password = request.form['password']  # In a real-world application, ensure this password is hashed
+        user = {"username": username, "password": password, "is_admin": False, "activities": []}
+        db.users.insert_one(user)
         return redirect(url_for('index'))
     return render_template('signup.html')
 
@@ -51,30 +50,34 @@ def signup():
 def login():
     if request.method == 'POST':
         username = request.form['username']
-        password = request.form['password']
-        user = User.query.filter_by(username=username, password=password).first()
+        password = request.form['password']  # Hash password before comparing, in a real application
+        user = db.users.find_one({"username": username, "password": password})
         if user:
-            session['user_id'] = user.id
-            log_activity(user.id, "User logged in")
-            analyze_user_behavior(user.id)
+            session['user_id'] = str(user['_id'])
+            log_activity(user['_id'], "User logged in")
+            # analyze_user_behavior(user['_id'])  # Implement this function for MongoDB if needed
             return redirect(url_for('dashboard'))
         else:
             flash('Invalid credentials')
     return render_template('login.html')
 
+# Logging activity function
 def log_activity(user_id, activity):
-    new_activity = UserActivity(user_id=user_id, activity=activity)
-    db.session.add(new_activity)
-    db.session.commit()
+    timestamp = datetime.utcnow()
+    db.users.update_one({"_id": user_id}, {"$push": {"activities": {"activity": activity, "timestamp": timestamp}}})
 
-def analyze_user_behavior(user_id):
-    activities = UserActivity.query.filter_by(user_id=user_id).all()
-    recent_logins = [act for act in activities if act.activity == "User logged in" and datetime.utcnow() - act.timestamp < timedelta(minutes=5)]
-    if len(recent_logins) > 3:
-        generate_alert("User " + str(user_id) + " logged in multiple times in a short period.")
+
+# def analyze_user_behavior(user_id):
+#     activities = UserActivity.query.filter_by(user_id=user_id).all()
+#     recent_logins = [act for act in activities if act.activity == "User logged in" and datetime.utcnow() - act.timestamp < timedelta(minutes=5)]
+#     if len(recent_logins) > 3:
+#         generate_alert("User " + str(user_id) + " logged in multiple times in a short period.")
 
 @app.route('/dashboard')
 def dashboard():
+    if 'user_id' not in session:
+        flash('You need to login first.')
+        return redirect(url_for('login'))
     return render_template('dashboard.html')
 
 @app.route('/logout')
@@ -85,36 +88,35 @@ def logout():
 
 @app.route('/bank')
 def bank():
-    user_id = session.get('user_id')
-    if not user_id:
+    if 'user_id' not in session:
         flash('You need to login first')
         return redirect(url_for('login'))
 
-    user = User.query.get(user_id)
-    if user and user.is_admin:
-        generate_alert("Admin user " + user.username + " accessed the bank page")
+    user_id = session.get('user_id')
+    user = db.users.find_one({"_id": ObjectId(user_id)})
+    if user and user.get('is_admin'):
+        generate_alert(f"Admin user {user['username']} accessed the bank page")
         return render_template('bank.html')
     else:
         flash('You do not have admin privileges to access this page')
         return redirect(url_for('dashboard'))
 
-    return redirect(url_for('dashboard'))
-
 @app.route('/escalate_privileges')
 def escalate_privileges():
-    user_id = session.get('user_id')
-    if user_id:
-        user = User.query.get(user_id)
-        if user:
-            user.is_admin = not user.is_admin
-            db.session.commit()
-            flash('Privileges have been escalated. Admin status: ' + str(user.is_admin))
-            generate_alert("Admin privileges changed for user " + user.username + ". New status: " + str(user.is_admin))
-        else:
-            flash('User not found.')
-    else:
+    if 'user_id' not in session:
         flash('You need to login first.')
-    
+        return redirect(url_for('login'))
+
+    user_id = session.get('user_id')
+    user = db.users.find_one({"_id": ObjectId(user_id)})
+    if user:
+        new_status = not user.get('is_admin', False)
+        db.users.update_one({"_id": ObjectId(user_id)}, {"$set": {"is_admin": new_status}})
+        flash(f'Privileges have been escalated. Admin status: {new_status}')
+        generate_alert(f"Admin privileges changed for user {user['username']}. New status: {new_status}")
+    else:
+        flash('User not found.')
+
     return redirect(url_for('dashboard'))
 
 def generate_alert(message):
